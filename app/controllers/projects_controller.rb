2bd9fe12a5bd86e6bbac49e12ff49cf37e095882
Adam D. White <adamwhite82@gmail.com>
2017-11-30 10:48:43 -0600

    misc

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :funding_goal, :start_date, :end_date, :active, :image_url, :about, :user_id)
+      params.require(:project).permit(:name, :description, :category, :funding_goal, :start_date, :end_date, :active, :image_url, :about, :user_id)
     end
 end
